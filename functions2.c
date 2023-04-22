c00a2b6d91963a1686aa983ac4316e57b93504bb
Mubymoore <omokayode2018@gmail.com>
2023-04-22 08:42:58 -0700

    Updated

--- functions2.c
+++ functions2.c
@@ -12,40 +12,40 @@
  * Return: Number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	char extra_c = 0, padd = ' ';
 	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
 	unsigned long num_addrs;
 	char map_to[] = "0123456789abcdef";
 	void *addrs = va_arg(types, void *);
-
+	
 	UNUSED(width);
 	UNUSED(size);
-
+	
 	if (addrs == NULL)
 		return (write(1, "(nil)", 5));
-
+	
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
 
 	num_addrs = (unsigned long)addrs;
-
+	
 	while (num_addrs > 0)
 	{
 		buffer[ind--] = map_to[num_addrs % 16];
 		num_addrs /= 16;
 		length++;
 	}
-if ((flags & F_ZERO) && !(flags & F_MINUS))
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
 		extra_c = '+', length++;
 	else if (flags & F_SPACE)
 		extra_c = ' ', length++;
-
+	
 	ind++;
-
+	
 	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
@@ -63,31 +63,31 @@ if ((flags & F_ZERO) && !(flags & F_MINUS))
  * Return: Number of chars printed
  */
 int print_non_printable(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
-
+	
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-
+	
 	if (str == NULL)
 		return (write(1, "(null)", 6));
-
+	
 	while (str[i] != '\0')
 	{
 		if (is_printable(str[i]))
 			buffer[i + offset] = str[i];
 		else
-offset += append_hexa_code(str[i], buffer, i + offset);
-
+			offset += append_hexa_code(str[i], buffer, i + offset);
+		
 		i++;
 	}
-
+	
 	buffer[i + offset] = '\0';
-
+	
 	return (write(1, buffer, i + offset));
 }
 
@@ -115,7 +115,7 @@ int print_reverse(va_list types, char buffer[],
 	UNUSED(size);
 
 	str = va_arg(types, char *);
-
+	
 	if (str == NULL)
 	{
 		UNUSED(precision);
@@ -124,10 +124,10 @@ int print_reverse(va_list types, char buffer[],
 	}
 	for (i = 0; str[i]; i++)
 		;
-for (i = i - 1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		char z = str[i];
-
+		
 		write(1, &z, 1);
 		count++;
 	}
@@ -184,4 +184,3 @@ int print_rot13string(va_list types, char buffer[],
 	}
 	return (count);
 }
-
